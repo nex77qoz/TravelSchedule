f9e659bcf259841ce996d46974ef0dac9a059613
MaxNextGaming <MaxNextGaming@gmail.com>
2025-03-29 15:25:07 +0300

    Улучшено отображение пользовательского соглашения

--- TravelSchedule/Services/WebKitService.swift
+++ TravelSchedule/Services/WebKitService.swift
@@ -5,16 +5,20 @@ struct WebView: UIViewRepresentable {
     let url: URL
 
     func makeUIView(context: Context) -> WKWebView {
-        WKWebView()
-    }
+        let config = WKWebViewConfiguration()
+        let controller = WKUserContentController()
 
-    func updateUIView(_ webView: WKWebView, context: Context) {
-        let lightDarkCSS = ":root { color-scheme: light dark; }"
-        let request = URLRequest(url: url)
-        guard let base64 = lightDarkCSS.data(using: .utf8)?.base64EncodedString() else { return }
-        webView.load(request)
-
-        let script = """
+        // CSS для light/dark темы
+        let lightDarkCSS = """
+        :root { color-scheme: light dark; }
+        body {
+            padding-left: 16px !important;
+            padding-right: 16px !important;
+            box-sizing: border-box;
+        }
+        """
+        if let base64 = lightDarkCSS.data(using: .utf8)?.base64EncodedString() {
+            let cssInjection = """
             javascript:(function() {
                 var parent = document.getElementsByTagName('head').item(0);
                 var style = document.createElement('style');
@@ -22,9 +26,34 @@ struct WebView: UIViewRepresentable {
                 style.innerHTML = window.atob('\(base64)');
                 parent.appendChild(style);
             })()
-        """
+            """
+            let cssScript = WKUserScript(source: cssInjection, injectionTime: .atDocumentEnd, forMainFrameOnly: false)
+            controller.addUserScript(cssScript)
+        }
 
-        let cssScript = WKUserScript(source: script, injectionTime: .atDocumentEnd, forMainFrameOnly: false)
-        webView.configuration.userContentController.addUserScript(cssScript)
+        // Скрипт для показа только нужного блока
+        let filterScript = WKUserScript(source: """
+        javascript:(function() {
+            document.body.querySelectorAll('body > *').forEach(el => {
+                if (!el.classList.contains('document') && !el.classList.contains('content__document') && !el.classList.contains('i-bem')) {
+                    el.style.display = 'none';
+                }
+            });
+            let docBlock = document.querySelector('.document.content__document.i-bem');
+            if (docBlock) {
+                document.body.innerHTML = '';
+                document.body.appendChild(docBlock);
+            }
+        })()
+        """, injectionTime: .atDocumentEnd, forMainFrameOnly: true)
+        controller.addUserScript(filterScript)
+
+        config.userContentController = controller
+        return WKWebView(frame: .zero, configuration: config)
+    }
+
+    func updateUIView(_ webView: WKWebView, context: Context) {
+        let request = URLRequest(url: url)
+        webView.load(request)
     }
 }
--- TravelSchedule/Views/UserAgreementView.swift
+++ TravelSchedule/Views/UserAgreementView.swift
@@ -1,25 +1,53 @@
 import SwiftUI
 
-
 struct AgreementView: View {
-    @State private var isPresentWebView = false
-    private let urlString = Bool.random()
-        ? "https://developer.apple.com/documentation/technologies"
-        : "https://yandex.ru/legal/practicum_offer"
+    @State private var simulateConnectionError = false
+    @State private var simulateServerError = false
+    @State private var isContinueTapped = false
 
+    private let urlString = "https://yandex.ru/legal/practicum_offer"
 
     var body: some View {
-        VStack {
-            switch Bool.random() {
-            case true:
-                if let url = URL(string: urlString) {
-                    WebView(url: url)
-                        .ignoresSafeArea(.all, edges: .bottom)
-                        .setCustomNavigationBar(title: "Пользовательское соглашение")
-                }
-            case false:
-                ErrorView(errorType: Bool.random() ? .connectionError : .serverError)
+        Group {
+            if isContinueTapped {
+                contentView()
+            } else {
+                setupView()
             }
+        }
+        .ignoresSafeArea(.all, edges: .bottom)
+    }
+
+    @ViewBuilder
+    private func setupView() -> some View {
+        VStack(spacing: 24) {
+            Toggle("Смоделировать ошибку подключения", isOn: $simulateConnectionError)
+            Toggle("Смоделировать ошибку сервера", isOn: $simulateServerError)
+
+            Button("Продолжить") {
+                isContinueTapped = true
+            }
+            .padding()
+            .background(Color.blue)
+            .foregroundColor(.white)
+            .cornerRadius(8)
+
+            Spacer()
+        }
+        .padding()
+    }
+
+    @ViewBuilder
+    private func contentView() -> some View {
+        if simulateConnectionError {
+            ErrorView(errorType: .connectionError)
+        } else if simulateServerError {
+            ErrorView(errorType: .serverError)
+        } else if let url = URL(string: urlString) {
+            WebView(url: url)
+                .setCustomNavigationBar(title: "Пользовательское соглашение")
+        } else {
+            ErrorView(errorType: .serverError)
         }
     }
 }
